164b5e7e3786abd5a7df4f7c14850bf85f9db1d9
Megan Day <meganeday7@gmail.com>
2021-04-26 21:38:29 -0500

    commit tree 77238f58ff2f80139ca11dbd6f667e148f281d38

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 24;
+let num4 = 6;
+
+let multiply = num1 * num2;
+
+let random = Number.parseInt(Math.random() +1);
+
+let mod = num3 / num4;
+
+let max = Math.max(20);
